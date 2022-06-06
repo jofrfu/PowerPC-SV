2bc2bd063f69519625fde11036459da8af909813
Jonas Fuhrmann <jofrfu@googlemail.com>
2022-06-06 12:23:43 +0200

    add/sub, mul and cmp decode.

--- src/instruction_decode.sv
+++ src/instruction_decode.sv
@@ -231,7 +231,7 @@ module instruction_decode(
                     decode_comb.fixed_point.rotate.alter_CR0 = M_form.Rc;
                 end
             31:
-                case(X_form.XO)
+                casez(X_form.XO)
                     // X Form load instructions
                     23: // lwzx
                         begin
@@ -334,6 +334,271 @@ module instruction_decode(
                         begin
                             decode_comb.fixed_point = decode_load_store_x_form(X_form, 2, 0, 0, 1);
                             decode_comb.fixed_point.execute = EXEC_STORE;
+                        end
+                    // Load/Store string word
+                    533: // lswx
+                        begin
+                            decode_comb.fixed_point = decode_load_store_x_form(X_form, 4, 0, 0, 0);
+                            decode_comb.fixed_point.execute = EXEC_LOAD_STRING;
+                        end
+                    597: // lswi
+                        begin
+                            if(X_form.RA == 0) begin
+                                decode_comb.fixed_point.control.op1_use_imm = 1;	// immediate is zero by default																			
+                            end else begin																				
+                                decode_comb.fixed_point.control.op1_reg_address = X_form.RA;						
+                            end
+                            
+                            if(X_form.NB == 0) begin
+                                decode_comb.fixed_point.control.op2_use_imm = 1;																		
+                                decode_comb.fixed_point.control.op2_immediate = 32;
+                            end else begin
+                                decode_comb.fixed_point.control.op2_use_imm = 1;
+                                decode_comb.fixed_point.control.op2_immediate = X_form.NB;						
+                            end
+                            
+                            decode_comb.fixed_point.control.op2_reg_address = X_form.RB;
+                            decode_comb.fixed_point.load_store.word_size = 4-1;								
+                            decode_comb.fixed_point.control.result_reg_address = X_form.RT;
+                            
+                            decode_comb.fixed_point.execute = EXEC_LOAD_STRING;
+                        end
+                    661: // stswx
+                        begin
+                            decode_comb.fixed_point = decode_load_store_x_form(X_form, 4, 0, 0, 0);
+                            decode_comb.fixed_point.execute = EXEC_STORE_STRING;
+                        end
+                    725: // stswi
+                        begin
+                            if(X_form.RA == 0) begin
+                                decode_comb.fixed_point.control.op1_use_imm = 1;	// immediate is zero by default																			
+                            end else begin																				
+                                decode_comb.fixed_point.control.op1_reg_address = X_form.RA;						
+                            end
+                            
+                            if(X_form.NB == 0) begin
+                                decode_comb.fixed_point.control.op2_use_imm = 1;																		
+                                decode_comb.fixed_point.control.op2_immediate = 32;
+                            end else begin
+                                decode_comb.fixed_point.control.op2_use_imm = 1;
+                                decode_comb.fixed_point.control.op2_immediate = X_form.NB;						
+                            end
+                            
+                            decode_comb.fixed_point.control.op2_reg_address = X_form.RB;
+                            decode_comb.fixed_point.load_store.word_size = 4-1;								
+                            decode_comb.fixed_point.control.result_reg_address = X_form.RT;
+                            
+                            decode_comb.fixed_point.execute = EXEC_STORE_STRING;
+                        end
+                    // XO Form Add/Sub instructions
+                    10'b?000001000: // 8 subfc, subfc., subfco, subfco.
+                        begin
+                            decode_comb.fixed_point.execute = EXEC_ADD_SUB;
+                            
+                            decode_comb.fixed_point.add_sub.subtract = 1;
+                            decode_comb.fixed_point.control.op1_reg_address = XO_form.RA;
+                            decode_comb.fixed_point.control.op2_reg_address = XO_form.RB;
+                            decode_comb.fixed_point.control.result_reg_address = XO_form.RT;
+                            decode_comb.fixed_point.add_sub.alter_CA = 1;
+                            decode_comb.fixed_point.add_sub.alter_CR0 = XO_form.Rc;
+                            decode_comb.fixed_point.add_sub.alter_OV = XO_form.OE;
+                        end
+                    10'b?000001010: // 10 addc, addc., addco, addco.
+                        begin
+                            decode_comb.fixed_point.execute = EXEC_ADD_SUB;
+                            
+                            decode_comb.fixed_point.control.op1_reg_address = XO_form.RA;
+                            decode_comb.fixed_point.control.op2_reg_address = XO_form.RB;
+                            decode_comb.fixed_point.control.result_reg_address = XO_form.RT;
+                            decode_comb.fixed_point.add_sub.alter_CA = 1;
+                            decode_comb.fixed_point.add_sub.alter_CR0 = XO_form.Rc;
+                            decode_comb.fixed_point.add_sub.alter_OV = XO_form.OE;
+                        end
+                    10'b?000101000: // 40 subf, subf., subfo, subfo.
+                        begin
+                            decode_comb.fixed_point.execute = EXEC_ADD_SUB;
+                            
+                            decode_comb.fixed_point.add_sub.subtract = 1;
+                            decode_comb.fixed_point.control.op1_reg_address = XO_form.RA;
+                            decode_comb.fixed_point.control.op2_reg_address = XO_form.RB;
+                            decode_comb.fixed_point.control.result_reg_address = XO_form.RT;
+                            decode_comb.fixed_point.add_sub.alter_CR0 = XO_form.Rc;
+                            decode_comb.fixed_point.add_sub.alter_OV = XO_form.OE;
+                        end
+                    10'b?001101000: // 104 neg, neg., nego, nego.
+                        begin
+                            decode_comb.fixed_point.execute = EXEC_ADD_SUB;
+                            
+                            decode_comb.fixed_point.add_sub.subtract = 1;
+                            decode_comb.fixed_point.control.op1_reg_address = XO_form.RA;
+                            decode_comb.fixed_point.control.op2_use_imm = 1; // immediate is zero by default
+                            decode_comb.fixed_point.control.result_reg_address = XO_form.RT;
+                            decode_comb.fixed_point.add_sub.alter_CR0 = XO_form.Rc;
+                            decode_comb.fixed_point.add_sub.alter_OV = XO_form.OE;
+                        end
+                    10'b?010001000: // 136 subfe, subfe., subfeo, subfeo.
+                        begin
+                            decode_comb.fixed_point.execute = EXEC_ADD_SUB;
+                            
+                            decode_comb.fixed_point.add_sub.subtract = 1;
+                            decode_comb.fixed_point.control.op1_reg_address = XO_form.RA;
+                            decode_comb.fixed_point.control.op2_reg_address = XO_form.RB;
+                            decode_comb.fixed_point.control.result_reg_address = XO_form.RT;
+                            decode_comb.fixed_point.add_sub.alter_CA = 1;
+                            decode_comb.fixed_point.add_sub.alter_CR0 = XO_form.Rc;
+                            decode_comb.fixed_point.add_sub.alter_OV = XO_form.OE;
+                            decode_comb.fixed_point.add_sub.add_CA = 1;
+                        end
+                    10'b?010001010: // 138 adde, adde., addeo, addeo.
+                        begin
+                            decode_comb.fixed_point.execute = EXEC_ADD_SUB;
+                            
+                            decode_comb.fixed_point.control.op1_reg_address = XO_form.RA;
+                            decode_comb.fixed_point.control.op2_reg_address = XO_form.RB;
+                            decode_comb.fixed_point.control.result_reg_address = XO_form.RT;
+                            decode_comb.fixed_point.add_sub.alter_CA = 1;
+                            decode_comb.fixed_point.add_sub.alter_CR0 = XO_form.Rc;
+                            decode_comb.fixed_point.add_sub.alter_OV = XO_form.OE;
+                            decode_comb.fixed_point.add_sub.add_CA = 1;
+                        end
+                    10'b?011001000: // 200 subfze, subfze., subfzeo, subfzeo.
+                        begin
+                            decode_comb.fixed_point.execute = EXEC_ADD_SUB;
+                            
+                            decode_comb.fixed_point.add_sub.subtract = 1;
+                            decode_comb.fixed_point.control.op1_reg_address = XO_form.RA;
+                            decode_comb.fixed_point.control.op2_use_imm = 1; // immediate is zero by default
+                            decode_comb.fixed_point.control.result_reg_address = XO_form.RT;
+                            decode_comb.fixed_point.add_sub.alter_CA = 1;
+                            decode_comb.fixed_point.add_sub.alter_CR0 = XO_form.Rc;
+                            decode_comb.fixed_point.add_sub.alter_OV = XO_form.OE;
+                            decode_comb.fixed_point.add_sub.add_CA = 1;
+                        end
+                    10'b?011001010: // 202 addze, addze., addzeo, addzeo.
+                        begin
+                            decode_comb.fixed_point.execute = EXEC_ADD_SUB;
+                            
+                            decode_comb.fixed_point.control.op1_reg_address = XO_form.RA;
+                            decode_comb.fixed_point.control.op2_use_imm = 1; // immediate is zero by default
+                            decode_comb.fixed_point.control.result_reg_address = XO_form.RT;
+                            decode_comb.fixed_point.add_sub.alter_CA = 1;
+                            decode_comb.fixed_point.add_sub.alter_CR0 = XO_form.Rc;
+                            decode_comb.fixed_point.add_sub.alter_OV = XO_form.OE;
+                            decode_comb.fixed_point.add_sub.add_CA = 1;
+                        end
+                    10'b?011101000: // 232 subfme, subfme., subfmeo, subfmeo.
+                        begin
+                            decode_comb.fixed_point.execute = EXEC_ADD_SUB;
+                            
+                            decode_comb.fixed_point.add_sub.subtract = 1;
+                            decode_comb.fixed_point.control.op1_reg_address = XO_form.RA;
+                            decode_comb.fixed_point.control.op2_use_imm = 1;
+                            decode_comb.fixed_point.control.op2_immediate = 32'hFFFFFFFF;
+                            decode_comb.fixed_point.control.result_reg_address = XO_form.RT;
+                            decode_comb.fixed_point.add_sub.alter_CA = 1;
+                            decode_comb.fixed_point.add_sub.alter_CR0 = XO_form.Rc;
+                            decode_comb.fixed_point.add_sub.alter_OV = XO_form.OE;
+                            decode_comb.fixed_point.add_sub.add_CA = 1;
+                        end
+                    10'b?011101010: // 234 addme, addme., addmeo, addmeo.
+                        begin
+                            decode_comb.fixed_point.execute = EXEC_ADD_SUB;
+                            
+                            decode_comb.fixed_point.control.op1_reg_address = XO_form.RA;
+                            decode_comb.fixed_point.control.op2_use_imm = 1;
+                            decode_comb.fixed_point.control.op2_immediate = 32'hFFFFFFFF;
+                            decode_comb.fixed_point.control.result_reg_address = XO_form.RT;
+                            decode_comb.fixed_point.add_sub.alter_CA = 1;
+                            decode_comb.fixed_point.add_sub.alter_CR0 = XO_form.Rc;
+                            decode_comb.fixed_point.add_sub.alter_OV = XO_form.OE;
+                            decode_comb.fixed_point.add_sub.add_CA = 1;
+                        end
+                    10'b?100001010: // 266 add, add., addo, addo.
+                        begin
+                            decode_comb.fixed_point.execute = EXEC_ADD_SUB;
+                            
+                            decode_comb.fixed_point.control.op1_reg_address = XO_form.RA;
+                            decode_comb.fixed_point.control.op2_reg_address = XO_form.RB;
+                            decode_comb.fixed_point.control.result_reg_address = XO_form.RT;
+                            decode_comb.fixed_point.add_sub.alter_CR0 = XO_form.Rc;
+                            decode_comb.fixed_point.add_sub.alter_OV = XO_form.OE;
+                        end
+                    // XO Form Mul instructions
+                    10'b?000001011: // 11 mulhwu, mulhwu.
+                        begin
+                            decode_comb.fixed_point.execute = EXEC_MUL;
+                            
+                            decode_comb.fixed_point.control.op1_reg_address = XO_form.RA;
+                            decode_comb.fixed_point.control.op2_reg_address = XO_form.RB;
+                            decode_comb.fixed_point.control.result_reg_address = XO_form.RT;
+                            decode_comb.fixed_point.mul.mul_higher = 1;
+                            decode_comb.fixed_point.mul.alter_CR0 = XO_form.Rc;
+                        end
+                    10'b?001001011: // 75 mulhw, mulhw., mulhwo, mulhwo.
+                        begin
+                            decode_comb.fixed_point.execute = EXEC_MUL;
+                            
+                            decode_comb.fixed_point.control.op1_reg_address = XO_form.RA;
+                            decode_comb.fixed_point.control.op2_reg_address = XO_form.RB;
+                            decode_comb.fixed_point.control.result_reg_address = XO_form.RT;
+                            decode_comb.fixed_point.mul.mul_signed = 1;
+                            decode_comb.fixed_point.mul.mul_higher = 1;
+                            decode_comb.fixed_point.mul.alter_CR0 = XO_form.Rc;
+                            decode_comb.fixed_point.mul.alter_OV = XO_form.OE;
+                        end
+                    10'b?011101011: // 235 mullw, mullw., mullwo, mullwo.
+                        begin
+                            decode_comb.fixed_point.execute = EXEC_MUL;
+                            
+                            decode_comb.fixed_point.control.op1_reg_address = XO_form.RA;
+                            decode_comb.fixed_point.control.op2_reg_address = XO_form.RB;
+                            decode_comb.fixed_point.control.result_reg_address = XO_form.RT;
+                            decode_comb.fixed_point.mul.mul_signed = 1;
+                            decode_comb.fixed_point.mul.alter_CR0 = XO_form.Rc;
+                            decode_comb.fixed_point.mul.alter_OV = XO_form.OE;
+                        end
+                    // XO Form Div instructions
+                    10'b?111001011: // 459 divwu, divwu., divwuo, divwuo.
+                        begin
+                            decode_comb.fixed_point.execute = EXEC_DIV;
+                            
+                            decode_comb.fixed_point.control.op1_reg_address = XO_form.RA;
+                            decode_comb.fixed_point.control.op2_reg_address = XO_form.RB;
+                            decode_comb.fixed_point.control.result_reg_address = XO_form.RT;
+                            decode_comb.fixed_point.div.alter_CR0 = XO_form.Rc;
+                            decode_comb.fixed_point.div.alter_OV = XO_form.OE;
+                        end
+                    10'b?111101011: // 491 divw, divw., divwo, divwo.
+                        begin
+                            decode_comb.fixed_point.execute = EXEC_DIV;
+                            
+                            decode_comb.fixed_point.control.op1_reg_address = XO_form.RA;
+                            decode_comb.fixed_point.control.op2_reg_address = XO_form.RB;
+                            decode_comb.fixed_point.control.result_reg_address = XO_form.RT;
+                            decode_comb.fixed_point.div.div_signed = 1;
+                            decode_comb.fixed_point.div.alter_CR0 = XO_form.Rc;
+                            decode_comb.fixed_point.div.alter_OV = XO_form.OE;
+                        end
+                    // X Form Cmp instructions
+                    0: // cmp
+                        begin
+                            decode_comb.fixed_point.execute = EXEC_COMPARE;
+                            
+                            decode_comb.fixed_point.control.op1_reg_address = X_form.RA;
+                            decode_comb.fixed_point.control.op2_reg_address = X_form.RB;
+                            decode_comb.fixed_point.control.result_reg_address = X_form.RT;
+                            decode_comb.fixed_point.cmp.cmp_signed = 1;
+                            decode_comb.fixed_point.cmp.BF = X_form.BF;
+                        end
+                    32: // cmpl
+                        begin
+                            // L is unused in 32 Bit implementations
+                            decode_comb.fixed_point.execute = EXEC_COMPARE;
+                            
+                            decode_comb.fixed_point.control.op1_reg_address = X_form.RA;
+                            decode_comb.fixed_point.control.op2_reg_address = X_form.RB;
+                            decode_comb.fixed_point.control.result_reg_address = X_form.RT;
+                            decode_comb.fixed_point.cmp.BF = X_form.BF;
                         end
                 endcase
         endcase
