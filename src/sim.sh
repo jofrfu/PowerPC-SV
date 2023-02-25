dda978d3e284b6d6a356e7331a9fce282441a6da
Jonas Fuhrmann <jofrfu@googlemail.com>
2023-02-25 15:12:35 +0100

    Update to sim script.

--- src/sim.sh
+++ src/sim.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-verilator -Wno-LITENDIAN --cc --trace --exe main.cpp ppc_top.sv
+verilator -Wno-LITENDIAN -Wno-ENUMVALUE --cc --trace --exe main.cpp ppc_top.sv
 make -j -C obj_dir -f Vppc_top.mk Vppc_top
 ./obj_dir/Vppc_top
