9c8f6cb82b64a405767d82e5e2ec7a84b5a6ebbc
Jonas Fuhrmann <jofrfu@googlemail.com>
2023-04-01 19:06:41 +0200

    Initial work on branch unit.

--- src/branch_unit.sv
+++ src/branch_unit.sv
@@ -21,12 +21,62 @@ module branch_unit(
     
     input logic input_valid,
     output logic input_ready,
-    input branch_decoded_t control,
+
+    input branch_inner_decode_t control,
+    input logic[0:31] cia_in,
+    input logic[0:3] cond_reg_in,
+    input logic[0:31] link_reg_in,
+    input logic[0:31] count_reg_in,
     
-    input logic[0:31] current_instruction_address,
-    output logic[0:31] next_instruction_address,
-    output logic speculative
+    output logic output_valid,
+    input logic output_ready,
+    output logic[0:31] nia_out,
+    output logic[0:31] link_reg_out,
+    output logic[0:31] count_reg_out,
+
+    // Marks all new incoming instructions as speculative
+    output logic speculative,
+    // Marks all marked instructions as non-sepculative
+    output logic clear_speculative,
+    // Flushes all speculative instructions in the pipeline
+    output logic flush_speculative
 );
+
+    logic[0:31] nia_ext;
+
+    always_comb
+    begin
+        case(control.operation)
+            BRANCH:
+            begin
+                // Sign extend
+                nia_ext = {6{control.LI[0]}, control.LI, 2'b0};
+
+                if(control.AA) begin
+                    nia_out = nia_ext;
+                end
+                else begin
+                    nia_out = nia_ext + cia_in;
+                end
+
+                // Don't care about link and count
+                link_reg_out = 32'bx;
+                count_reg_out = 32'bx;
+            end
+            BRANCH_CONDITIONAL:
+            begin
+
+            end
+            BRANCH_CONDITIONAL_LINK:
+            begin
+
+            end
+            BRANCH_CONDITIONAL_COUNT:
+            begin
+
+            end
+        endcase
+    end
 
 
 
